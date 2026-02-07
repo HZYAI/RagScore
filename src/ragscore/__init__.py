6f013f4a9e6d8ce9cbdd54fc7aaa24635850cd17
RAGScore Developer <developer@ragscore.local>
2026-02-07 15:08:10 +1100

    add Python 3.13 support

--- src/ragscore/__init__.py
+++ src/ragscore/__init__.py
@@ -12,7 +12,7 @@ Usage:
 For more information, see: https://github.com/HZYAI/RagScore
 """
 
-__version__ = "0.6.4"
+__version__ = "0.6.5"
 __author__ = "RAGScore Team"
 
 # Auto-patch asyncio for notebook environments on import
